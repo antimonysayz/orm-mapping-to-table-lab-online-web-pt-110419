c7d42d4fcbe76214f86f1b96e448efcba3d6809b
Anthony Mai <anthony.mai530@gmail.com>
2020-01-30 01:38:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,9 +30,9 @@ end
 
 def save
   sql = <<-SQL
-  INSERT into students (name, grade) VALUES (? ?)
+  INSERT into students (name, grade) VALUES (?, ?)
   SQL
   DB[:conn].execute(sql, self.name, self.grade)
 end
-  
+
 end
